59661bc000b9c0c2831f3fe739ce098d236bce6b
uyencfi <77076051+uyencfi@users.noreply.github.com>
2021-03-26 07:26:49 +0800

    Add folder for 'accounts' app
    'accounts' is the app to manage user sign up, log in, log out. This
    app makes heavy use of Django's built-in authentication and
    authorisation system.

---
+++ accounts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+# models of accounts.py
+#custom user model, display username instead of id
+
+# class LibraryUser(User):
+#   pass
